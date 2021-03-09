4deede7b74848779210fdaf7d79f48b78983d8f2
Frank Nolasco Velasquez <49081234+FranksitoKawai@users.noreply.github.com>
2021-03-09 18:52:39 -0500

    ROUTES

--- src/controllers/api.controllers.js
+++ src/controllers/api.controllers.js
@@ -1,5 +1,6 @@
 const ApiCtrl = {};
 var mssql = require("mssql");
+const bcrypt = require("bcrypt-nodejs");
 const { executeQuery, executeAxios } = require("../scripts/querys");
 
 ApiCtrl.login = (req, res, next) => {
@@ -20,6 +21,20 @@ ApiCtrl.ListaColaborador = (req, res, next) => {
   executeQuery("dbo.ListaTrabajadores", res, next);
 };
 
+ApiCtrl.InsertarTrabajador = (req, res, next) => {
+  const { numero_documento, turno, id_puesto, id_sucursal } = req.body;
+  executeQuery(
+    `dbo.insertarTrabajador '${numero_documento}','${turno}','${id_puesto}','${id_sucursal}'`,
+    res,
+    next
+  );
+};
+
+ApiCtrl.BuscarTrabajador = (req, res, next) => {
+  const  { dni } = req.body
+  executeQuery(`dbo.buscarTrabajador '${dni}'`,res,next)
+}
+
 ApiCtrl.ListaUsuarios = (req, res, next) => {
   let request = new mssql.Request();
   request.query(`dbo.ListaUsuarios`, function (err, result) {
@@ -30,6 +45,25 @@ ApiCtrl.ListaUsuarios = (req, res, next) => {
       estado: el.estado === 0 ? "INACTIVO" : "ACTIVO",
     }));
     res.send(data);
+  });
+};
+
+ApiCtrl.InsertarUsuario = (req, res, next) => {
+  const { Usuario, Clave, id_trabajador, Rol } = req.body;
+  bcrypt.genSalt(12, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(Clave, salt, null, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      executeQuery(
+        `dbo.insertarUsuario '${Usuario}','${hash}','${id_trabajador}','${Rol}'`,
+        res,
+        next
+      );
+    });
   });
 };
 
@@ -47,6 +81,28 @@ ApiCtrl.ListaIncidencias = (req, res, next) => {
 
 ApiCtrl.ListaPersonas = (req, res, next) => {
   executeQuery("dbo.ListaPersonas", res, next);
+};
+
+ApiCtrl.BuscarPersona = (req, res, next) => {
+  const { dni } = req.body;
+  executeQuery(`dbo.buscarPersona '${dni}'`, res, next);
+};
+
+ApiCtrl.InsertarPersona = (req, res, next) => {
+  const {
+    nombre,
+    numero_documento,
+    RUC,
+    fecha_nacimiento,
+    genero,
+    nacionalidad,
+    distrito_nacimiento,
+  } = req.body;
+  executeQuery(
+    `dbo.insertarPersona '${nombre}','${numero_documento}','${fecha_nacimiento}','','${genero}','${nacionalidad}','${distrito_nacimiento}','${RUC}'`,
+    res,
+    next
+  );
 };
 
 ApiCtrl.ListaCiudad = async (req, res, next) => {
@@ -91,5 +147,10 @@ ApiCtrl.ListaDepartamentos = (req, res, next) => {
 ApiCtrl.ListarSucursales = (req, res, next) => {
   executeQuery("dbo.ListarSucursales", res, next);
 };
+
+ApiCtrl.ListarRoles = (req, res, next) => {
+  executeQuery("dbo.ListarRoles", res, next);
+};
+
 
 module.exports = ApiCtrl;
--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -13,21 +13,31 @@ const {
   ListaDistritos,
   ListaDepartamentos,
   ListarSucursales,
+  InsertarPersona,
+  BuscarPersona,
+  InsertarTrabajador,
+  InsertarUsuario,
+  ListarRoles,
+  BuscarTrabajador,
 } = require("../controllers/api.controllers");
 const {
   ListarDireccionesXPersona,
   ListarContactosXPersona,
 } = require("../controllers/personaController");
 const routes = Router();
-
 routes.post("/log-in", login);
 routes.post("/ordenes-de-servicio/listar", ListaOrdenServicio);
 routes.post("/colaboradores/listar", ListaColaborador);
+routes.post("/trabajadores/insertar",InsertarTrabajador)
+routes.post("/trabajadores/buscar",BuscarTrabajador)
 routes.post("/usuarios/listar", ListaUsuarios);
+routes.post("/usuarios/insertar", InsertarUsuario);
 routes.post("/puestos-de-trabajo/listar", ListaPuestoTrabajo);
 routes.post("/mensajeros/listar", ListaMensajero);
 routes.post("/incidencias/listar", ListaIncidencias);
 routes.post("/personas/listar", ListaPersonas);
+routes.post("/personas/buscar", BuscarPersona);
+routes.post("/personas/insertar", InsertarPersona);
 routes.post("/personas/direcciones/listar", ListarDireccionesXPersona);
 routes.post("/personas/contacto/listar", ListarContactosXPersona);
 routes.post("/ciudad/listar", ListaCiudad);
@@ -35,5 +45,5 @@ routes.post("/provincias/listar", ListaProvincias);
 routes.post("/distritos/listar", ListaDistritos);
 routes.post("/departamentos/listar", ListaDepartamentos);
 routes.post("/sucursales/listar", ListarSucursales);
-
+routes.post("/roles/listar",ListarRoles)
 module.exports = routes;
